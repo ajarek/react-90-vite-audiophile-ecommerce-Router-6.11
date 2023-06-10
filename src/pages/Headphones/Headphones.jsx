2e66b6f3f0a580b2f407320930c9a60f96c7b7e9
Ajarek <ajarek@poczta.onet.pl>
2023-06-10 23:11:36 +0200

    added page headphones

--- src/pages/Headphones/Headphones.jsx
+++ src/pages/Headphones/Headphones.jsx
@@ -1,8 +1,44 @@
 import './Headphones.css'
-
+import data from '../../assets/data/products.json'
+import Button from '../../components/Button/Button'
+import Section from '../../components/Section/Section'
 const Headphones = () => {
   return (
-    <div className='headphones'>Headphones</div>
+    <div className='headphones'>
+      <div className='title'>
+        <h1>HEADPHONES</h1>
+      </div>
+      <div className='headphones-list'>
+        {data &&
+          data.products
+            .filter((el) => el.category === 'headphones')
+            .reverse()
+            .map((product) => {
+              return (
+              <div key={product.id} className='card'>
+                <div className="left-card">
+                  
+                    <img src={product.categoryImage.desktop} alt={product.name} />
+                  
+                </div>
+                <div className="right-card">
+                    {product.new?<div className='new'>NEW PRODUCT</div>: null}
+                  <div className="name">
+                    <h2>{product.name}</h2>
+                  </div>
+                  <div className="description">{product.description}</div>
+                  <Button
+                    label='SEE PRODUCT'
+                    background={'#d87d4a'}
+                    color={'#fff'}
+                  />
+                </div>
+
+              </div>)
+            })}
+      </div>
+      <Section/>
+    </div>
   )
 }
 
