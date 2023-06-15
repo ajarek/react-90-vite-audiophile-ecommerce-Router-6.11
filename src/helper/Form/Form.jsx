3e08c4cd96bfa1d0262d4f7cb960f43affda0f7c
Ajarek <ajarek@poczta.onet.pl>
2023-06-16 00:34:31 +0200

    added checkout

--- src/helper/Form/Form.jsx
+++ src/helper/Form/Form.jsx
@@ -6,9 +6,15 @@ import './Form.css'
 import { useEffect } from 'react'
 export const Form = ({ onSubmit }) => {
   const schema = yup.object().shape({
-    categories: yup.string().required(),
-    title: yup.string().required(),
-    author: yup.string().required(),
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    phone: yup.string().required(),
+    address: yup.string().required(),
+    zip: yup.string().required(),
+    city: yup.string().required(),
+    country: yup.string().required(),
+    money: yup.string().required(),
+    pin: yup.string().required(),
   })
 
   const {
@@ -24,9 +30,15 @@ export const Form = ({ onSubmit }) => {
   useEffect(() => {
     if (formState.isSubmitSuccessful) {
       reset({
-        categories: '',
-        title: '',
-        author: '',
+        name: '',
+        email: '',
+        phone: '',
+        address: '',
+        zip: '',
+        city: '',
+        country: '',
+        money: '',
+        pin: '',
       })
     }
   }, [formState, reset])
@@ -34,33 +46,90 @@ export const Form = ({ onSubmit }) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className='wrapper-input'>
+        <label htmlFor='name'>Name</label>
         <input
           type='text'
-          placeholder='Categories...'
-          {...register('categories')}
+          placeholder='Alexei Ward'
+          {...register('name')}
         />
-        <p>{errors?.categories?.message}</p>
+        <p>{errors?.name?.message}</p>
       </div>
       <div className='wrapper-input'>
+        <label htmlFor='email'>Email Address</label>
         <input
-          type='text'
-          placeholder='Book title...'
-          {...register('title')}
+          type='email'
+          placeholder='alexei@gmail.com'
+          {...register('email')}
         />
-        <p>{errors.title?.message}</p>
+        <p>{errors.email?.message}</p>
       </div>
       <div className='wrapper-input'>
+        <label htmlFor='phone'>Phone Number</label>
+        <input
+          type='phone'
+          placeholder='+1 202-555-0136'
+          {...register('phone')}
+        />
+        <p>{errors.phone?.message}</p>
+      </div>
+      <div className='wrapper-input'>
+        <label htmlFor='phone'>Your Address</label>
         <input
           type='text'
-          placeholder='Author...'
-          {...register('author')}
+          placeholder='1137 Williams Avenue'
+          {...register('address')}
         />
-        <p>{errors.author?.message}</p>
+        <p>{errors.address?.message}</p>
+      </div>
+      <div className='wrapper-input'>
+        <label htmlFor='phone'>ZIP Code</label>
+        <input
+          type='text'
+          placeholder='10001'
+          {...register('zip')}
+        />
+        <p>{errors.zip?.message}</p>
+      </div>
+      <div className='wrapper-input'>
+        <label htmlFor='phone'>City</label>
+        <input
+          type='text'
+          placeholder='New York'
+          {...register('city')}
+        />
+        <p>{errors.city?.message}</p>
+      </div>
+      <div className='wrapper-input'>
+        <label htmlFor='phone'>Country</label>
+        <input
+          type='country'
+          placeholder='United States'
+          {...register('country')}
+        />
+        <p>{errors.country?.message}</p>
+      </div>
+      <div className='wrapper-input'>
+        <label htmlFor='phone'>e-Money Number</label>
+        <input
+          type='text'
+          placeholder='238521996'
+          {...register('money')}
+        />
+        <p>{errors.money?.message}</p>
+      </div>
+      <div className='wrapper-input'>
+        <label htmlFor='phone'>e-Money PIN</label>
+        <input
+          type='phone'
+          placeholder='6891'
+          {...register('pin')}
+        />
+        <p>{errors.pin?.message}</p>
       </div>
       <div className='wrapper-input'>
         <input
           type='submit'
-          value='ADD BOOK'
+          value='SUBMIT'
         />
       </div>
     </form>
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,8 +1,22 @@
+import Footer from '../../components/Footer/Footer'
+import {Form} from '../../helper/Form/Form'
 import './Checkout.css'
 
 const Checkout = () => {
+  const handleForm = (data) => {
+    console.log(data);
+  }
   return (
-    <div className='checkout'>Checkout</div>
+    <div className='checkout'>
+      <div className='checkout-wrapper'>
+        <div className='form'>
+          <h2>CHECKOUT</h2>
+          <Form onSubmit={handleForm}/>
+        </div>
+        <div className='summary'><h2>SUMMARY</h2></div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
